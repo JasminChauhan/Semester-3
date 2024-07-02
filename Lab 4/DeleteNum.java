8ed4f881339da9bd50f1ee3f51ee63387c3d8699
Jasmin Chauhan <146665572+JasminChauhan@users.noreply.github.com>
2024-07-02 23:55:07 +0530

    Delete Lab 4 directory

--- Lab 4/DeleteNum.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class DeleteNum {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int arr[] = new int[];
-    }
-}
