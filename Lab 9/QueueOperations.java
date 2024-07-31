ba5cdd6c1ba1dd68333a5f16539e562f495fb43a
Jasmin Chauhan <146665572+JasminChauhan@users.noreply.github.com>
2024-07-31 13:38:14 +0530

    Update QueueOperations.java

--- Lab 9/QueueOperations.java
+++ Lab 9/QueueOperations.java
@@ -61,12 +61,42 @@ public class QueueOperations{
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of queue: ");
         int size = sc.nextInt();
+        System.out.println("enter choice: \n 1 for enqueue \n 2 for dequeue \n 3 for display \n 4 for exit");
+        int choice = sc.nextInt();
 
         Queue q = new Queue(size);
-        q.enqueue(10);
-        q.enqueue(20);
-        q.enqueue(30);
-        q.dequeue();
-        q.display();
+
+
+        while(true){
+            switch(choice){
+                case 1:
+                    System.out.println("enter number to enqueue");
+                    int data = sc.nextInt();
+                    q.enqueue(data);
+                    break;
+
+                case 2:
+                    q.dequeue();
+                    break;
+
+                case 3:
+                    q.display();
+                    break;
+
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice ");
+            }
+        }
+            
+
+        // q.enqueue(10);
+        // q.enqueue(20);
+        // q.enqueue(30);
+        // q.dequeue();
+        // q.display();
     }
 }
