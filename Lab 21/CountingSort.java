d161a663b3267ae69559b0cab07469caa8e0e2f2
Jasmin Chauhan <146665572+JasminChauhan@users.noreply.github.com>
2024-09-11 21:01:56 +0530

    Update CountingSort.java

--- Lab 21/CountingSort.java
+++ Lab 21/CountingSort.java
@@ -1,36 +1,30 @@
 public class CountingSort {
     public static void main(String[] args) {
-        int []arr = {1,2,3,4,5};
+        int []arr = {1, 2, 3, 4, 5};
         int max = arr[0];
         
-        for (int i = 1; i < arr.length - 1; i++) {
-            if(arr[i] > arr[i + 1]) {
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
                 max = arr[i];
             }  
         }
-        int []arr1 = new int[max];
+        
+        int []arr1 = new int[max + 1];
 
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 1; j < arr1.length; j++) {
-                if (arr1[j] == i) {
-                    arr1[i]++;
-                }
-            }
+        for (int i = 0; i < arr.length; i++) {
+            arr1[arr[i]]++;
         }
-        int sum = arr1[0];
+        
         for (int i = 1; i < arr1.length; i++) {
-            sum += arr1[i];
-            arr1[i] = sum;
+            arr1[i] += arr1[i - 1];
         }
+        
         int[] result = new int[arr.length];
 
         for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = 1; j < arr1.length; j++) {
-                if (arr[i] == j) {
-                    result[arr1[j]] = arr[i];
-                }   
-            }
+            result[--arr1[arr[i]]] = arr[i];
         }
+       
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
